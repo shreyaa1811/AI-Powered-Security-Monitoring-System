78b6c8182e9f0564f174780f4ac261f4657c3e3d
Shreyaa Murali <f20230300@dubai.bits-pilani.ac.in>
2026-06-16 12:56:13 +0400

    Update file paths for log data

--- generators/combine.py
+++ generators/combine.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-normal_df = pd.read_csv("c:\\Users\\18Shr\\Desktop\\Security System\\data\\normal_logs.csv")
-attack_df = pd.read_csv("c:\\Users\\18Shr\\Desktop\\Security System\\data\\attack_logs.csv")
+normal_df = pd.read_csv("C:\path\to\your\project\data\normal_logs.csv")
+attack_df = pd.read_csv("C:\path\to\your\project\data\attack_logs.csv")
 
 all_logs = pd.concat([normal_df,attack_df],ignore_index=True)
-all_logs.to_csv("c:\\Users\\18Shr\\Desktop\\Security System\\data\\combined_logs.csv",index=False)
+all_logs.to_csv("C:\path\to\your\project\data\combined_logs.csv",index=False)
 
 print("All logs combined : ",all_logs.shape)
